ba6a99f45f1de46a6aae74a3a662510fc8845f8e
Juan Sebastian <juanllano93@gmail.com>
2020-04-29 03:09:01 +0000

    add task 11

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,13 +1,3 @@
 #!/usr/bin/env python3
 def pow(a, b):
-    if b == 0:
-        result = 1
-    elif b < 0:
-        result = 1
-        for i in range(b, 0):
-            result /= a
-    else:
-        result = 1
-        for i in range(b):
-            result *= a
-    return result
+    return a ** b
