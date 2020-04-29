4be9d4d2dd08ae32e725ba633de9a24bf28d7967
Juan Sebastian <juanllano93@gmail.com>
2020-04-29 01:19:36 +0000

    add task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,8 @@
 for i in range(0, 100):
         decena = i / 10
         unidad = i % 10
-        if unidad > decena:
+        if unidad > decena and i < 89:
                 print('{:02d}, '.format(i), end='')
+        elif i == 89:
+                print('{:02d}'.format(i), end='')
 print()
