8b7d05c07baf5503f63fb6f01c20748a97693434
Juan Sebastian <juanllano93@gmail.com>
2020-04-29 00:44:03 +0000

    add task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-        print('{:d}, '.format(i), end="")
+        print('{:02d}'.format(i), end='')
+        if i < 99:
+                print(', ', end='')
 print()
