4a2347b5021934440f92209518b77e393ada75b1
Juan Sebastian <juanllano93@gmail.com>
2020-05-01 05:06:23 +0000

    add task 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,18 +5,17 @@ if __name__ == "__main__":
     if len(argv) < 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    if argv[2] != "+" or argv[2] != "-" or argv[2] != "*" or argv[2] != "/":
+    if argv[2] != "+" and argv[2] != "-" and argv[2] != "*" and argv[2] != "/":
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-    calc = 0
     a = int(argv[1])
     b = int(argv[3])
-    if argv[2] != "+":
-        add(a, b)
-    if argv[2] != "-":
-        sub(a, b)
-    if argv[2] != "*":
-        mul(a, b)
-    if argv[2] != "/":
-        div(a, b)
+    if argv[2] == "+":
+        calc = add(a, b)
+    if argv[2] == "-":
+        calc = sub(a, b)
+    if argv[2] == "*":
+        calc = mul(a, b)
+    if argv[2] == "/":
+        calc = div(a, b)
     print("{} {} {} = {}".format(a, argv[2], b, calc))
