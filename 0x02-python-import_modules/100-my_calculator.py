77f94b5cdb15fe459ec4b82f400b99696db0d6ab
Juan Sebastian <juanllano93@gmail.com>
2020-05-01 05:10:41 +0000

    add task

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
-from sys import argv, exit
+import sys
 if __name__ == "__main__":
-    if len(argv) < 3:
+    if len(sys.argv) < 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
-    if argv[2] != "+" and argv[2] != "-" and argv[2] != "*" and argv[2] != "/":
+        sys.exit(1)
+    if sys.argv[2] != "+" and sys.argv[2] != "-" and sys.argv[2] != "*" and sys.argv[2] != "/":
         print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
-    a = int(argv[1])
-    b = int(argv[3])
-    if argv[2] == "+":
+        sys.exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    if sys.argv[2] == "+":
         calc = add(a, b)
-    if argv[2] == "-":
+    if sys.argv[2] == "-":
         calc = sub(a, b)
-    if argv[2] == "*":
+    if sys.argv[2] == "*":
         calc = mul(a, b)
-    if argv[2] == "/":
+    if sys.argv[2] == "/":
         calc = div(a, b)
-    print("{} {} {} = {}".format(a, argv[2], b, calc))
+    print("{} {} {} = {}".format(a, sys.argv[2], b, calc))
