49dc84eb2c5918422dd3c52da7b7750029f98ca8
Juan Sebastian <juanllano93@gmail.com>
2020-05-05 22:55:58 +0000

    add task 2

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,11 @@
+#!/bin/usr/python3
+def replace_in_list(my_list, idx, element):
+        index = 0
+        lenght = len(my_list)
+        if idx < 0 or idx > lenght:
+                return None
+        for i in range(lenght):
+                if index == idx:
+                        my_list[index] = element
+                        return my_list
+                index += 1
