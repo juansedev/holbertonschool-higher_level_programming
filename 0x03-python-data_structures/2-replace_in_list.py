8b18b1acb45d0464a121e232760917c4de515027
Juan Sebastian <juanllano93@gmail.com>
2020-05-05 22:59:47 +0000

    add task 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,7 +2,7 @@
 def replace_in_list(my_list, idx, element):
         index = 0
         lenght = len(my_list)
-        if idx < 0 or idx > lenght:
+        if idx < 0 or idx > lenght - 1:
                 return my_list
         for i in range(lenght):
                 if index == idx:
