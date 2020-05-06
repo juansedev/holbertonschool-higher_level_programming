036731eed1add290b5c5f8608d6a33ff61315311
Juan Sebastian <juanllano93@gmail.com>
2020-05-06 04:24:14 +0000

    add task 11

--- 0x03-python-data_structures/10-main.py
+++ 0x03-python-data_structures/10-main.py
@@ -6,6 +6,5 @@ list_result = divisible_by_2(my_list)
 
 i = 0
 while i < len(list_result):
-	    print("{:d} {:s} divisible by 2".format(my_list[i], "is" if list_result[i] else "is not"))
-	        i += 1
-
+    print("{:d} {:s} divisible by 2".format(my_list[i], "is" if list_result[i] else "is not"))
+    i += 1
