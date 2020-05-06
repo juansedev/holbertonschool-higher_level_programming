5fc65bc85ba867a35e1607c2898dc6be1bf9e534
Juan Sebastian <juanllano93@gmail.com>
2020-05-06 02:01:32 +0000

    add task5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -5,6 +5,4 @@ def no_c(my_string):
                 for i in my_string:
                         if i != 'c' and i != 'C':
                                 new_string = new_string + i
-                        else:
-                                new_string = new_string + "_"
                 return new_string
