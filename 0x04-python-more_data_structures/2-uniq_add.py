fd167942bd4c1943db4bac1aa95f2caf5d6d3462
Juan Sebastian <juanllano93@gmail.com>
2020-05-07 22:31:57 +0000

    add task 2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,16 +1,3 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-        add = 0
-        index1 = 0
-        if len(my_list) == 0:
-                return
-        for i in my_list:
-                number = my_list[index1]
-                add += number
-                index2 = index1 + 1
-                for j in range(index2, len(my_list)):
-                        if number == j:
-                                my_list[index2] = 0
-                        index2 += 1
-                index1 += 1
-        return add
+        return sum(set(my_list))
