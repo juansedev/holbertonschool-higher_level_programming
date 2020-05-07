f3eec3dbc95a137b7c7e3763db602d3ffc56318f
Juan Sebastian <juanllano93@gmail.com>
2020-05-07 22:26:45 +0000

    add task 2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -2,6 +2,8 @@
 def uniq_add(my_list=[]):
         add = 0
         index1 = 0
+        if len(my_list) == 0:
+                return
         for i in my_list:
                 number = my_list[index1]
                 add += number
