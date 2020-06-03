1f28ca3060328c58c418cafc2bbbb57107b05237
Juan Sebastian Llano <juanllano93@gmail.com>
2020-06-03 02:25:14 +0000

    fix file 3

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -2,7 +2,7 @@
 """In this module, it creates a function to validate the instance class"""
 
 
-def is_same_class(obj, a_class):
+def is_kind_of_class(obj, a_class):
     """ This function that returns True if the object is an instance of,
     or if the object is an instance of a class that inherited from, the
     specified class ; otherwise False
