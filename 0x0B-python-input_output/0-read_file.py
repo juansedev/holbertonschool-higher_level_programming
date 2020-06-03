4d4417b2958cb2d740b169c82c9ce4e18b5de783
Juan Sebastian Llano <juanllano93@gmail.com>
2020-06-03 22:08:18 +0000

    fix 0 task , close() file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,8 +3,7 @@
 
 
 def read_file(filename=""):
-    """This  function that reads a text file (UTF8) and
-    prints it to stdout:
-    """
+    """This  function that reads a text file (UTF8) and"""
     with open("my_file_0.txt", mode='r', encoding="utf-8") as my_file:
         print(my_file.read())
+    my_file.close()
