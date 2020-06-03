c88be2489f8120fd796469639f2a4c89a608d0a4
Juan Sebastian Llano <juanllano93@gmail.com>
2020-06-03 03:49:22 +0000

    add task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" In this module it create a Class BaseGeometry"""
+
+
+class BaseGeometry:
+    """This a class BaseGeometry"""
+    def area(self):
+        """Method for compute the area"""
+        raise Exception("area() is not implemented")
