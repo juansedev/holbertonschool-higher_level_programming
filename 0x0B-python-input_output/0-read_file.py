a7968bb2ef1f1642bd359eb845a2b3992fe98a91
Juan Sebastian Llano <juanllano93@gmail.com>
2020-06-03 22:09:48 +0000

    fix filename

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,5 @@
 
 def read_file(filename=""):
     """This  function that reads a text file (UTF8) and"""
-    with open("my_file_0.txt", mode='r', encoding="utf-8") as my_file:
+    with open(filename, mode='r', encoding="utf-8") as my_file:
         print(my_file.read())
-    my_file.close()
