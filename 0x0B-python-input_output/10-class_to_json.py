36b7a221fad166b71a85f93ca875ac298fd692de
Juan Sebastian Llano <juanllano93@gmail.com>
2020-06-04 02:27:46 +0000

    add task 10

---
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Write a function that returns the dictionary description with simple data
+    structure(list, dictionary, string, integer and boolean) for JSON
+    serialization of an object:
+"""
+
+
+def class_to_json(obj):
+    """Returns the dictionary of object
+
+    Arguments:
+        obj {[type]} -- object to convert
+    """
+    obj.__dict__
