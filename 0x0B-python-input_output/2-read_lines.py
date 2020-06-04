a5bdc40b739f805b6f583f874318131d1111e80e
Juan Sebastian Llano <juanllano93@gmail.com>
2020-06-04 01:39:16 +0000

    add task 9

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -16,7 +16,6 @@ def read_lines(filename="", nb_lines=0):
         f_list = my_file.readlines()
         if nb_lines <= 0 or nb_lines > len(f_list):
             nb_lines = len(f_list)
-        i = 0
         for i in range(nb_lines):
             print(f_list[i], end='')
     return i + 1
