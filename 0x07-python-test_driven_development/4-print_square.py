e94163bcafa7bceec973f4f5786b5759a4029f89
Juan Sebastian Llano <juanllano93@gmail.com>
2020-06-05 20:47:19 +0000

    update file 4-print_square.py

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -9,8 +9,6 @@ def print_square(size):
     Args:
         size ([type]): [description]
     """
-    if type(size) is float and size < 0:
-        raise TypeError("size must be an integer")
     if type(size) is not int:
         raise TypeError("size must be an integer")
     if size < 0:
