aa9e8ebe3bc57769ddabd1ebe36bc53381505646
Juan Sebastian Llano <juanllano93@gmail.com>
2020-06-06 03:28:42 +0000

    add task 13

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""in this module it implement a class student"""
+
+
+class Student:
+    """Class Student"""
+    def __init__(self, first_name, last_name, age):
+        """Constructor method
+
+        Arguments:
+            first_name {str} -- [description]
+            last_name {str} -- [description]
+            age {str} -- [description]
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        s_dict = self.__dict__
+
+        if type(attrs) is list and all(
+                isinstance(items, str) for items in attrs):
+            new_dict = {}
+            for name in attrs:
+                if name in s_dict:
+                    new_dict[name] = s_dict[name]
+            return new_dict
+
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        if json is not None:
+            self.__dict__ = json
