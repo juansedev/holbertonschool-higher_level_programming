487a75f7e988a165369e4674bfc911afa930d3e5
Juan Sebastian Llano <juanllano93@gmail.com>
2020-06-06 01:57:39 +0000

    fix task 6

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -14,17 +14,17 @@ class TestMaxInteger(unittest.TestCase):
 
 
     def empty_list(self):
+        """Empty list case"""
         self.assertEqual(max_integer([]), None)
 
     def not_a_list(self):
+        """ Not a number list cases"""
         self.assertEqual(max_integer(['a', 'b', 'c']), 'c')
         self.assertEqual(max_integer(('a', 'b', 'c')), 'c')
 
     def number_list(self):
+        """Number list cases"""
         self.assertEqual(max_integer([-3, -1, -2]), -1)
         self.assertEqual(max_integer([8 , 4, 5, 2]), 8)
         self.assertEqual(max_integer([-1 , 4, -3, 2]), 4)
-
-
-    def same_seacrh(self):
         self.assertEqual(max_integer([4, 4, 4, 4]), 4)
