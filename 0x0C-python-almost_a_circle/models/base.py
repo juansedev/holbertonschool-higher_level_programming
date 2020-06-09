2cd5efb424a7437a11edf793d30e9e9a332b982f
Juan Sebastian Llano <juanllano93@gmail.com>
2020-06-09 21:23:38 +0000

    add task 15 y 16

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,9 @@
 """ Module for a Class Base"""
 
 
+import json
+
+
 class Base:
     """This is a Class Base"""
 
@@ -19,3 +22,31 @@ class Base:
             self.id = Base.__nb_objects
         else:
             self.id = id
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returns the JSON string representation of list_dictionaries.
+           (Serialization)
+
+        Args:
+            list_dictionaries (list): A list of dictinaries
+        """
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes the JSON string representation of list_objs to a file:
+
+        Args:
+            list_objs (list): A list of objects
+        """
+        filename = "{}.json".format(cls.__name__)
+        list_dicts = []
+        with open(filename, mode="w", encoding="utf-8") as my_file:
+            if list_objs is None:
+                my_file.write("[]")
+            else:
+                list_dicts = [item.to_dictionary() for item in list_objs]
+                my_file.write(cls.to_json_string(list_dicts))
