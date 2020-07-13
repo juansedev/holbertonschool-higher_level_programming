e5779f17ecf9c991e1668170ad9d8b550357558e
Juan Sebastian Llano <juanllano93@gmail.com>
2020-07-13 22:13:05 +0000

    update task 4

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,8 +1,7 @@
 -- Create a new table
 -- use `mysql` database
 -- table `first_table`
-USE hbtn_0c_0;
-CREATE TABLE first_table (
+CREATE TABLE IF NO EXISTS first_table (
 id INT(11),
 name VARCHAR(256)
 );
