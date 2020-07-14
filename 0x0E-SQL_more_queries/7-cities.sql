cdd6e21ddec18dd114929b9d56d51107c938f82b
Juan Sebastian Llano <juanllano93@gmail.com>
2020-07-14 21:15:07 +0000

    update order of keys in table cities

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,10 +2,9 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities(
-    id INT AUTO_INCREMENT NOT NULL,
-    name VARCHAR(256) NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     state_id INT NOT NULL,
-    PRIMARY KEY (id),
+    name VARCHAR(256) NOT NULL,
     FOREIGN KEY (state_id)
         REFERENCES states(id)
 );
