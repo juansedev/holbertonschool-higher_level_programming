9fc1e47d1d839deb1506ae72a1cf93c102fc709c
Juan Sebastian Llano <juanllano93@gmail.com>
2020-07-14 21:05:55 +0000

    update task 7 with id as primary key

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,7 +3,7 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities(
     id INT UNIQUE AUTO_INCREMENT NOT NULL,
-    name VARCHAR(256),
+    name VARCHAR(256) NOT NULL,
     state_id INT NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (state_id)
