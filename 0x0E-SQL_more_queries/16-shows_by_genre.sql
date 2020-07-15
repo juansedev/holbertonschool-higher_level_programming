9cda9f81eb608bba69523c82c0df46535e899349
Juan Sebastian Llano <juanllano93@gmail.com>
2020-07-15 01:14:53 +0000

    add task 16

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -3,4 +3,4 @@ SELECT tv_s.title AS title, tv_ge.name as name
 FROM tv_shows AS tv_s
 LEFT JOIN tv_show_genres AS tv_g ON tv_s.id = tv_g.show_id 
 LEFT JOIN tv_genres AS tv_ge ON tv_ge.id = tv_g.genre_id 
-ORDER BY title,name;
+ORDER BY title,name ASC;
