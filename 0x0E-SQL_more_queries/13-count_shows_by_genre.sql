0bfed0ac43874637eb0246b15c478e93c1e5cd8b
Juan Sebastian Llano <juanllano93@gmail.com>
2020-07-15 00:03:58 +0000

    add task 13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- list all shows contained in hbtn_0d_tshows
-SELECT tv_g.name, COUNT(*) AS c 
+SELECT tv_g.name AS genre, COUNT(*) AS number_of_shows
 FROM tv_genres AS tv_g 
 LEFT JOIN tv_show_genres AS tv_s ON tv_g.id = tv_s.genre_id 
-GROUP BY tv_g.name
-ORDER BY c DESC;
+GROUP BY genre
+ORDER BY number_of_shows DESC;
 
