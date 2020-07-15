66624af6c681ef77ed1471caae058c525a8e0b31
Juan Sebastian Llano <juanllano93@gmail.com>
2020-07-15 01:11:18 +0000

    add task 16

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- script that uses the hbtn_0d_tvshows database to lists all shows
+SELECT tv_s.title AS title, tv_ge.name as name
+FROM tv_shows AS tv_s
+LEFT JOIN tv_show_genres AS tv_g ON tv_s.id = tv_g.show_id 
+LEFT JOIN tv_genres AS tv_ge ON tv_ge.id = tv_g.genre_id 
+ORDER BY title,name;
