a52f753d24608f958487ec953589fe6f43f9efdd
Juan Llano <juanllano93@gmail.com>
2020-08-18 21:52:09 +0000

    add task 5

---
+++ 0x10-python-network_0/4-header.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Script to get the Contentn-Lenght a throught curl command
+curl -s "$1" -X GET -H "X-HolbertonSchool-User-Id: 98"
---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Script to get the Contentn-Lenght a throught curl command
+curl -s "$1" -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
