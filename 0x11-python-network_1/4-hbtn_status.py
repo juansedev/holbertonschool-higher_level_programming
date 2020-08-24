a3944060ab93bb816b2dea68fe6434509edad888
llanojs <juanllano93@gmail.com>
2020-08-24 18:52:21 -0500

    add task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 """ Script to display an specific Header http"""
 
-import urllib.request
+import requests
 
 
 if __name__ == "__main__":
-
-    try:
-        with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-            print("Body response:")
-            print("\t- type: {}".format(type(response.read().decode('utf-8'))))
-            print("\t- content: {}".format(response.read().decode('utf-8')))
-    except urllib.error.HTTPError as e:
-        print("Error code: {}".format(e.code))
+    req = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(req.text)))
+    print("\t- content: {}".format(req.text))
