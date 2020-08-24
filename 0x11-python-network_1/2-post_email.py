543638a31eed1e34c75f46dbf4014db7eb831ace
llanojs <juanllano93@gmail.com>
2020-08-24 18:10:09 -0500

    add 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
+""" Script to display an specific Header http"""
 
-from urllib import request
-from sys import argv
+import urllib.request
+import urllib.parse
+import sys
 
-if argv[1]:
-    url = argv[1]
-    req = request.Request(url)
-    with request.urlopen(req) as response:
-        print(response.info()['X-Request-Id'])
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    values = {}
+    values['email'] = email
+    url_values = urllib.parse.urlencode(values)
+    url_values = url_values.encode('ascii')
+    with urllib.request.urlopen(url, url_values) as response:
+        rint(response.read().decode('utf-8'))
