d3cfc93671aedcbdbf4d28b77979ed74082b2041
Juan Sebastian <juanllano93@gmail.com>
2020-09-16 02:00:48 +0000

    add task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/* script that prints a square */
+if (isNaN(process.argv[2])) {
+  console.log('Missing number of occurrences');
+} else if (process.argv[2] > 0) {
+  for (let i = 0; i < process.argv[2]; i++) {
+    console.log('X'.repeat(process.argv[2]));
+  }
+}
