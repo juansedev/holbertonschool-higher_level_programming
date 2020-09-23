5e2a968349935039d0f2f0b492fa6126435e28fc
llanojs <juanllano93@gmail.com>
2020-09-23 04:46:59 +0000

    add task 5

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+/*  script that reads and prints the content of a file */
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/';
+
+request(url, function (err, res, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const reqJson = JSON.parse(body).results;
+    let count = 0;
+    for (const film of reqJson) {
+      for (const character of film.characters) {
+        if (character.includes('/18/')) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
+  }
+});
